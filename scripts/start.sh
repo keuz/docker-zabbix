fe195b80787e3ac815de9baf8ec904568234632f
Alexey Bogdanenko <abogdanenko@localhost.localdomain>
2014-08-25 14:55:35 +0400

    Fix database access problem for zabbix@localhost

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,43 +1,31 @@
 #!/bin/bash
 
-if [ ! -f /mysql-configured ]; then 
- 	/sbin/service mysqld restart 
+if [ ! -f /mysql-configured ]; then
+    service mysqld start
 
- 	/usr/bin/mysql_upgrade
+    mysql_upgrade
 
-	 sleep 10s
+    sleep 10s
 
-	 MYSQL_PASSWORD="mypassword"
+    PASS="mypassword"
+    echo "mysql admin password: $PASS"
 
-	 echo "mysql root and admin password: $MYSQL_PASSWORD"
+    mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';"
+    mysql -uroot -e "CREATE DATABASE zabbix CHARACTER SET utf8;"
+    zabbix_mysql_v="/usr/share/zabbix-mysql"
+    mysql -uroot zabbix < "${zabbix_mysql_v}/schema.sql"
+    mysql -uroot zabbix < "${zabbix_mysql_v}/images.sql"
+    mysql -uroot zabbix < "${zabbix_mysql_v}/data.sql"
+    mysql -uroot -e "CREATE USER 'zabbix'@'%' IDENTIFIED BY 'zabbix'"
+    mysql -uroot -e "CREATE USER 'zabbix'@'localhost' IDENTIFIED BY 'zabbix'"
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'%'"
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost'"
 
-	 echo "$MYSQL_PASSWORD" > /mysql-root-pw.txt
+    mysqladmin -uroot shutdown
+    #service mysqld stop
 
-	 mysqladmin -uroot password $MYSQL_PASSWORD 
-
-	 mysql -uroot -p"$MYSQL_PASSWORD" -e "INSERT INTO mysql.user (Host,User,Password) VALUES('%','admin',PASSWORD('${MYSQL_PASSWORD}'));"
-
-	 mysql -uroot -p"$MYSQL_PASSWORD" -e "GRANT ALL ON *.* TO 'admin'@'%';"
-
-	 mysqladmin -uroot -p"$MYSQL_PASSWORD" create zabbix
-
-	 mysql -uroot -p"$MYSQL_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS zabbix CHARACTER SET utf8;"
-
-	 zabbix_mysql_v="/usr/share/zabbix-mysql"
-
-	 mysql -uroot -D zabbix -p"$MYSQL_PASSWORD" < "${zabbix_mysql_v}/schema.sql"
-	 mysql -uroot -D zabbix -p"$MYSQL_PASSWORD" < "${zabbix_mysql_v}/images.sql"
-	 mysql -uroot -D zabbix -p"$MYSQL_PASSWORD" < "${zabbix_mysql_v}/data.sql"
-
-	 mysql -uroot -p"$MYSQL_PASSWORD" -e "INSERT INTO mysql.user (Host,User,Password) VALUES('localhost','zabbix',PASSWORD('zabbix'));"
-
-	 mysql -uroot -p"$MYSQL_PASSWORD" -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON zabbix.* TO 'zabbix'@'%';"
-
-	 /sbin/service mysqld stop
-
-	 touch /mysql-configured
+    touch /mysql-configured
 fi
 
-passwd -d root
-
-/usr/bin/monit -d 10 -Ic /etc/monitrc
+monit -d 10 -Ic /etc/monitrc
